e7d2c3db46626b9be52e16a93ee92790b02c50c7
garthdp <garthdp4@gmail.com>
2024-09-12 15:36:30 +0200

    updated

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,6 +12,8 @@ namespace PROG_POE.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+
+        // used for uploading files
         private readonly IWebHostEnvironment _webHost;
         List<Report> reports = new List<Report>();
         List<User> users = new List<User>();
@@ -24,24 +26,33 @@ namespace PROG_POE.Controllers
             _webHost = webHost;
         }
 
+        //shows home page
         public IActionResult Index()
         {
+            // redirects user to sign in if not signed in
             if(HttpContext.Session.GetString("SessionUser") == "")
             {
                 return RedirectToAction("SignIn");
             }
             return View();
         }
+
+        // loads user profile
         public IActionResult Profile()
         {
+            // redirects user to sign in if not signed in
             if (HttpContext.Session.GetString("SessionUser") == "")
             {
                 return RedirectToAction("SignIn");
             }
+
             User user = new User();
             List<User> users = new List<User>();
+
+            //gets saved users from session
             users = JsonConvert.DeserializeObject<List<User>>(HttpContext.Session.GetString("Users"));
 
+            //finds user from list and gets users details
             foreach (User usera in users)
             {
                 if (usera.Username == HttpContext.Session.GetString("SessionUser"))
@@ -50,11 +61,15 @@ namespace PROG_POE.Controllers
                     break;
                 }
             }
+
+            // assigns user details to page
             ViewBag.Username = user.Username;
             ViewBag.Level = user.Level;
             ViewBag.Experience = user.Experience;
             return View();
         }
+
+        //signs user out and redirects to signin
         public ActionResult SignOut()
         {
             HttpContext.Session.SetString("SessionUser", "");
@@ -62,22 +77,32 @@ namespace PROG_POE.Controllers
         }
         public IActionResult Leaderboard()
         {
+            // redirects user to sign in if not signed in
             if (HttpContext.Session.GetString("SessionUser") == "")
             {
                 return RedirectToAction("SignIn");
             }
             List<User> users = new List<User>();
+
+            //gets list of users from session
             users = JsonConvert.DeserializeObject<List<User>>(HttpContext.Session.GetString("Users"));
+
+            //sorts users by highest level to lowest level
             var sortedUsersByLevel = users.OrderByDescending(x => x.Level);
             ViewBag.Users = sortedUsersByLevel;
             return View();
         }
+
+        // shows list of reports
         public IActionResult ReportList()
         {
+            // redirects user to sign in if not signed in
             if (HttpContext.Session.GetString("SessionUser") == "")
             {
                 return RedirectToAction("SignIn");
             }
+
+            // gets list of reports from session
             reports = JsonConvert.DeserializeObject<List<Report>>(HttpContext.Session.GetString("Reports"));
             ViewBag.Reports = reports;
             return View();
@@ -91,10 +116,14 @@ namespace PROG_POE.Controllers
              Made by: Noor Codelogics
              Link: https://www.youtube.com/watch?v=6bPeFO10GN4
              */
+
             reports.Add(new Report("Location Test", "Roads", "Lots of potholes", "Example of pothole"));
+
+            //makes report list for session on sign in 
             string reportsString = JsonConvert.SerializeObject(reports);
             HttpContext.Session.SetString("Reports", reportsString);
 
+            //makes user list for session on sign in 
             users.Add(new User("Garth", "password", 5, 0));
             users.Add(new User("Ryan", "password", 3, 50));
             users.Add(new User("Seth", "password", 2, 0));
@@ -104,12 +133,17 @@ namespace PROG_POE.Controllers
 
             return View();
         }
+
+        //signs in user
         [HttpPost]
         public IActionResult SignIn(User user)
         {
+            // gets list of users
             List<User> users = new List<User>();
             users = JsonConvert.DeserializeObject<List<User>>(HttpContext.Session.GetString("Users"));
             bool found = false;
+
+            // checks if inputted user info is correct or not
             foreach(User usera in users)
             {
                 if(usera.Username == user.Username && usera.Password == user.Password)
@@ -128,12 +162,16 @@ namespace PROG_POE.Controllers
             }
         }
 
+        // loads create report page
         public IActionResult CreateReport()
         {
+            //makes report list for session on sign in 
             if (HttpContext.Session.GetString("SessionUser") == "")
             {
                 return RedirectToAction("SignIn");
             }
+
+            //fills categorys selectlist
             List<SelectListItem> categories = new List<SelectListItem>();
             SelectListItem category1 = new SelectListItem();
             category1.Text = "Sanitation";
@@ -155,9 +193,11 @@ namespace PROG_POE.Controllers
             return View();
         }
 
+        // creates report and saves it to list
         [HttpPost]
         public async Task<IActionResult> CreateReport(Report report, IFormFile SingleFile)
         {
+            //makes report list for session on sign in 
             if (HttpContext.Session.GetString("SessionUser") == "")
             {
                 return RedirectToAction("SignIn");
@@ -187,20 +227,34 @@ namespace PROG_POE.Controllers
                 await SingleFile.CopyToAsync(stream);
             }
 
+            // gets reports list
             List<Report> reports = new List<Report>();
             reports = JsonConvert.DeserializeObject<List<Report>>(HttpContext.Session.GetString("Reports"));
+
+            // adds reports to list
             reports.Add(report);
+
+            // saves list to session again with new report
             string reportsString = JsonConvert.SerializeObject(reports);
             HttpContext.Session.SetString("Reports", reportsString);
 
+            // gets lists of users
             users = JsonConvert.DeserializeObject<List<User>>(HttpContext.Session.GetString("Users"));
             currentUser = users.Where(x => x.Username == HttpContext.Session.GetString("SessionUser")).First();
+
+            // adds experience to users account on report completion
             currentUser.addExperience(50);
 
+            // finds user
             int index = users.FindIndex(x => x.Username == HttpContext.Session.GetString("SessionUser"));
+
+            //removes out of date user info
             users.RemoveAt(index);
+
+            // adds updated info to user
             users.Add(currentUser);
 
+            // saves updated list to session
             string usersString = JsonConvert.SerializeObject(users);
             HttpContext.Session.SetString("Users", usersString);
 
